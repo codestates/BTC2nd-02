505b5c4d1c58be04d9b38b4dd5783d6e001f3bbf
koeunchorong <koasis03@gmail.com>
2022-05-10 13:46:55 +0900

    클라이언트 네비 바 완성

---
+++ explorer/src/App.js
@@ -0,0 +1,24 @@
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Main from "./pages/Main.js";
+import React, { useState } from "react";
+import Nav from "./components/Nav";
+import Blockpage from "./pages/blockpage.js";
+import Txpage from "./pages/txpage.js";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Nav></Nav>
+        <Routes>
+          <Route default path="/" element={<Main />} />
+          <Route default path="/block" element={<Blockpage />} />
+          <Route default path="/tx" element={<Txpage />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
