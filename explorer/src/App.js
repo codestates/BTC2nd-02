34f743b5e66fe0c64bc42ca7eb1c6fb094401491
koeunchorong <koasis03@gmail.com>
2022-05-13 18:07:33 +0900

    디자인 변경 및 목록 생성

--- explorer/src/App.js
+++ explorer/src/App.js
@@ -1,21 +1,22 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Main from "./pages/Main.js";
 import React, { useState } from "react";
 import Nav from "./components/Nav";
-import Blockpage from "./pages/blockpage.js";
-import Txpage from "./pages/txpage.js";
+import Accountpage from "./pages/Accountpage";
+import Txpage from "./pages/txpage";
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <Router>
+        <Nav />
         <Routes>
-          <Route default path="/" element={<Main />} />
-          <Route path="/block" element={<Blockpage />} />
-          <Route path="/tx" element={<Txpage />} />
+          <Route path="/" element={<Main />} />
+          <Route path="/account" element={<Accountpage />} />
+          <Route default path="/tx" element={<Txpage />} />
         </Routes>
-      </BrowserRouter>
+      </Router>
     </div>
   );
 }
