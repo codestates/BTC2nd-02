8a3597cf27072affcb17c09ee9ed7bac8b53b14d
koeunchorong <koasis03@gmail.com>
2022-05-13 12:08:32 +0900

    디자인 변경과 검색기능 구현

--- explorer/src/pages/Main.js
+++ explorer/src/pages/Main.js
@@ -2,8 +2,24 @@ import React, { useState, useEffect } from "react";
 import { BsSearch } from "react-icons/bs";
 import "../App.css";
 import CaverExtKAS from "caver-js-ext-kas";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import Logo from "../components/img/KLAETLogo.png";
 
-export default function Main() {
+const theme = createTheme();
+
+export default function SignIn() {
   const accessKeyId = "KASKREJNW0ITLFCGVX5H5HSL";
   const secretAccessKey = "70TItBV28va0q8rGGwuu3vjmHX0M3PrErlmnsG54";
   const chainId = 1001;
@@ -58,69 +74,91 @@ export default function Main() {
   }, [accountHis]);
 
   return (
-    <div>
-      <div className="searchtitle">트랜잭션 해시 검색</div>
-      <div className="searchbar">
-        <input
-          type="text"
-          onChange={onChangeTxHash}
-          placeholder="트랜잭션 해시를 입력하세요."
-          className="inputtxt"
-        ></input>
-        <BsSearch
-          size="25"
-          className="searchicon"
-          type="button"
-          onClick={findTxHash}
-          color="#0c61fe"
-        />
-      </div>
-      <div className="list">
-        <div className="texttitle">검색하신 트랜잭션의 정보입니다.</div>
-        <div className="textq">block Number : </div>
-        <div className="text">{txArr.blockNumber}</div>
-        <div className="textq">Transfer Type : </div>
-        <div className="text">{txArr.transferType}</div>
-        <div className="textq">To : </div>
-        <div className="text">{txArr.to}</div>
-        <div className="textq">From : </div>
-        <div className="text">{txArr.from}</div>
-        <div className="textq">Transaction Hash :</div>
-        <div className="text">{txArr.transactionHash}</div>
-      </div>
-      <div className="searchtitle">어카운트 정보 검색</div>
-      <div className="searchbar">
-        <input
-          type="text"
-          onChange={onChangeAccount}
-          placeholder="계정 주소를 입력하세요."
-          className="inputtxt"
-        ></input>
-        <BsSearch
-          size="25"
-          className="searchicon"
-          type="button"
-          onClick={findAccount}
-          color="#0c61fe"
-        />
-      </div>
-      <div className="list">
-        <table>
-          <div className="texttitle">
-            가장 최근에 일어난 account 트랜잭션입니다.
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Typography component="h1" variant="h5">
+            <img className="logo" src={Logo} alt="logo" width="80"></img>
+          </Typography>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Transaction Hash"
+            name="TxHash"
+            autoComplete="TxHash"
+            autoFocus
+            onChange={onChangeTxHash}
+          />
+          <div className="list">
+            <div className="texttitle">검색하신 트랜잭션의 정보입니다.</div>
+            <div className="textq">block Number : </div>
+            <div className="text">{txArr.blockNumber}</div>
+            <div className="textq">Transfer Type : </div>
+            <div className="text">{txArr.transferType}</div>
+            <div className="textq">To : </div>
+            <div className="text">{txArr.to}</div>
+            <div className="textq">From : </div>
+            <div className="text">{txArr.from}</div>
+            <div className="textq">Transaction Hash :</div>
+            <div className="text">{txArr.transactionHash}</div>
           </div>
-          <div className="textq">block Number : </div>
-          <div className="text">{accountHis.blockNumber}</div>
-          <div className="textq">Transfer Type : </div>
-          <div className="text">{accountHis.transferType}</div>
-          <div className="textq">Transaction Hash :</div>
-          <div className="text">{accountHis.transactionHash}</div>
-          <div className="textq">From : </div>
-          <div className="text">{accountHis.from}</div>
-          <div className="textq">To : </div>
-          <div className="last">{accountHis.to}</div>
-        </table>
-      </div>
-    </div>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+            onClick={findTxHash}
+          >
+            SEARCH
+          </Button>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="Account Address"
+            label="Address"
+            type="Address"
+            id="Address"
+            autoComplete="Address"
+            onChange={onChangeAccount}
+          />
+          <div className="list">
+            <div className="texttitle">
+              가장 최근에 일어난 account 트랜잭션입니다.
+            </div>
+            <div className="textq">block Number : </div>
+            <div className="text">{accountHis.blockNumber}</div>
+            <div className="textq">Transfer Type : </div>
+            <div className="text">{accountHis.transferType}</div>
+            <div className="textq">Transaction Hash :</div>
+            <div className="text">{accountHis.transactionHash}</div>
+            <div className="textq">From : </div>
+            <div className="text">{accountHis.from}</div>
+            <div className="textq">To : </div>
+            <div className="last">{accountHis.to}</div>
+          </div>
+
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+            onClick={findAccount}
+          >
+            SEARCH
+          </Button>
+        </Box>
+      </Container>
+    </ThemeProvider>
   );
 }
