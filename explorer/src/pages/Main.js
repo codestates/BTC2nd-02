772f1f9d1f83af04666ed7cf2eb031cff5cf19ff
koeunchorong <koasis03@gmail.com>
2022-05-13 10:45:14 +0900

    검색 기능 구현 완료

--- explorer/src/pages/Main.js
+++ explorer/src/pages/Main.js
@@ -14,6 +14,8 @@ export default function Main() {
   const [txHash, setTxHash] = useState("");
   const [account, setAccount] = useState("");
   const [accountHis, setAccountHis] = useState("");
+  let isFindA = 0;
+  let isFindT = 0;
 
   const onChangeTxHash = (e) => {
     setTxHash(e.target.value);
@@ -26,25 +28,38 @@ export default function Main() {
     console.log(result.items[0]);
     setTxArr(result.items[0]);
   };
+
+  useEffect(() => {
+    isFindT = isFindT + 1;
+    if (isFindT > 1) {
+      setTxArr(txArr);
+      console.log(txArr);
+    }
+  }, [txArr]);
+
   const onChangeAccount = (e) => {
     setAccount(e.target.value);
   };
+
   const findAccount = async () => {
     const result = await caver.kas.tokenHistory.getTransferHistoryByAccount(
       account
     );
-    console.log(result.items[0]);
     setAccountHis(result.items[0]);
     console.log(accountHis);
   };
+
   useEffect(() => {
-    setAccountHis(accountHis);
-    console.log(accountHis);
+    isFindA = isFindA + 1;
+    if (isFindA > 1) {
+      setAccountHis(accountHis);
+      console.log(accountHis);
+    }
   }, [accountHis]);
 
   return (
     <div>
-      <div>트랜잭션 해시 검색</div>
+      <div className="searchtitle">트랜잭션 해시 검색</div>
       <div className="searchbar">
         <input
           type="text"
@@ -57,9 +72,23 @@ export default function Main() {
           className="searchicon"
           type="button"
           onClick={findTxHash}
+          color="#0c61fe"
         />
       </div>
-      <div>어카운트 정보 검색</div>
+      <div className="list">
+        <div className="texttitle">검색하신 트랜잭션의 정보입니다.</div>
+        <div className="textq">block Number : </div>
+        <div className="text">{txArr.blockNumber}</div>
+        <div className="textq">Transfer Type : </div>
+        <div className="text">{txArr.transferType}</div>
+        <div className="textq">To : </div>
+        <div className="text">{txArr.to}</div>
+        <div className="textq">From : </div>
+        <div className="text">{txArr.from}</div>
+        <div className="textq">Transaction Hash :</div>
+        <div className="text">{txArr.transactionHash}</div>
+      </div>
+      <div className="searchtitle">어카운트 정보 검색</div>
       <div className="searchbar">
         <input
           type="text"
@@ -72,7 +101,25 @@ export default function Main() {
           className="searchicon"
           type="button"
           onClick={findAccount}
+          color="#0c61fe"
         />
+      </div>
+      <div className="list">
+        <table>
+          <div className="texttitle">
+            가장 최근에 일어난 account 트랜잭션입니다.
+          </div>
+          <div className="textq">block Number : </div>
+          <div className="text">{accountHis.blockNumber}</div>
+          <div className="textq">Transfer Type : </div>
+          <div className="text">{accountHis.transferType}</div>
+          <div className="textq">Transaction Hash :</div>
+          <div className="text">{accountHis.transactionHash}</div>
+          <div className="textq">From : </div>
+          <div className="text">{accountHis.from}</div>
+          <div className="textq">To : </div>
+          <div className="last">{accountHis.to}</div>
+        </table>
       </div>
     </div>
   );
